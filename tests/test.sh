a8cee7acdfbbdb00aab059e6429b8fcf2db91e34
Erik Clarke <ecl@mail.med.upenn.edu>
2016-05-13 12:46:11 -0400

    debugging travis-specific problems

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -x
-source deactivate 
+
 # Ensure we can activate the environment
 export PATH=$PATH:$HOME/miniconda3/bin
 source activate sunbeam
@@ -10,6 +10,8 @@ command -v snakemake
 if [ ! -d tests/sunbeam-data ]; then
     git clone --depth=1 https://github.com/eclarke/sunbeam-data tests/sunbeam-data
 fi
+
+mkdir local/db
 
 # Running snakemake without options should give a list of samples
 # Here we just check to ensure it outputted a sample name
