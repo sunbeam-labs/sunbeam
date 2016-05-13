3586087cbe79660fefcbb76f5acc582a9adfef38
Erik Clarke <ecl@mail.med.upenn.edu>
2016-05-13 11:40:57 -0400

    sourcing test script instead of executing it

--- tests/test.sh
+++ tests/test.sh
@@ -5,6 +5,7 @@ set -x
 # Ensure we can activate the environment
 export PATH=$PATH:$HOME/miniconda3/bin
 source activate sunbeam
+command -v snakemake
 
 if [ ! -d tests/sunbeam-data ]; then
     git clone --depth=1 https://github.com/eclarke/sunbeam-data tests/sunbeam-data
@@ -12,5 +13,5 @@ fi
 
 # Running snakemake without options should give a list of samples
 # Here we just check to ensure it outputted a sample name
-snakemake --configfile=tests/test-config.yml | grep HUP3D04
+snakemake --configfile=tests/test-config.yml | grep HUP3D04 && return 0 || return 1
 
