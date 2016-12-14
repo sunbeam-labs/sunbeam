54edba2e921a63c39d2fe21bd4699e2fe990df75
Erik <ecl@mail.med.upenn.edu>
2016-12-14 11:26:46 -0500

    testing tempdir for testing

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#set -e
-#set -x
+# Bash flags: Do not commit to repo with these commented out
+set -e # Stop on errors
+set -x # Echo commands
 
 # Ensure we can activate the environment
 export PATH=$PATH:$HOME/miniconda3/bin
@@ -10,33 +11,51 @@ source activate sunbeam
 command -v snakemake
 
 # Temporary
-pip install git+https://github.com/zhaoc1/decontam.git
+pip install git+https://github.com/eclarke/decontam.git
 
-pushd tests
+# Set up paths
+ROOT=`pwd`
+TEMPDIR=`mktemp -d`
+mkdir -p $TEMPDIR/data_files
 
-mkdir -p local
+function cleanup {
+    # Remove temporary directory if it exists
+    # (must be careful with rm -rf and variables)
+    [ -z ${TEMPDIR+x} ] || rm -rf "$TEMPDIR"
+}
 
+# Calls cleanup when the script exits
+trap cleanup EXIT
+
+# Copy data into the temporary directory
+cp -r raw $TEMPDIR
+cp -r truncated_taxonomy $TEMPDIR
+cp -r indexes $TEMPDIR
+python generate_dummy_data.py $TEMPDIR
+
+# Create a version of the config file customized for this tempdir
+sed "s|TEMPDIR|$TEMPDIR|g" test_config.yml > $TEMPDIR/tmp_config.yml
+sed -i "s|HOME|$HOME|g" $TEMPDIR/tmp_config.yml
+
+pushd $TEMPDIR
+
+pwd
+ls -ahl
 # Generate testing data: data_files
-if [ ! -d data_files ]; then
-	mkdir data_files
-	python generate_dummy_data.py
-fi
+# mkdir -p data_files
+# python generate_dummy_data.py
 
-# Deploy kranken and blast databases
-if [ ! -d local/db/bacteria ]; then
-	bash deploy_kraken_db.sh
-fi
+# # Deploy kranken and blast databases
+# bash deploy_kraken_db.sh
 
-if [ ! -d local/blast ]; then
-	bash deploy_blast_db.sh
-fi
+# bash deploy_blast_db.sh
 
 popd
 
 # Running snakemake
-echo "Now testing the snakemake: "
-snakemake --configfile=tests/test_config.yml
+echo "Now testing snakemake: "
+#snakemake --configfile=tests/test_config.yml
 
 # Here we just check to ensure it hits the expected genome
 echo "Now checking whether we hit the expected genome:"
-grep 'NC_006347.1' tests/sunbeam_output/annotation/summary/dummybfragilis.tsv  && return 0 || return 1
+#grep 'NC_006347.1' tests/sunbeam_output/annotation/summary/dummybfragilis.tsv  && return 0 || return 1
