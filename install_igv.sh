8e8cf1ee0f4ed08de8992796ddeb21bf360c5b75
Jesse Connell <ancon@upenn.edu>
2017-04-07 12:55:58 -0400

    downgrade installed IGV from 2.3.92 to 2.3.68
    The latest IGV version generates blank image panes when run with a batch
    script, so this rolls back to a known working version.

--- install_igv.sh
+++ install_igv.sh
@@ -5,12 +5,12 @@ set -e
 # $HOME/IGV will still need to be separately added to PATH for this to work.
 install_igv() {
     DIR=$HOME
-    IGV_VER=2.3.92
+    IGV_VER=2.3.68
     wget http://data.broadinstitute.org/igv/projects/downloads/IGV_${IGV_VER}.zip
     unzip IGV_${IGV_VER}.zip -d $DIR
     ln -s IGV_$IGV_VER $DIR/IGV
     # A symlink will confuse igv.sh so I'm using a wrapper script instead
-    echo -e "#!/bin/bash\ncd $DIR/IGV && igv.sh \$@" > $DIR/IGV/igv
+    echo -e "#!/bin/bash\ncd $DIR/IGV && ./igv.sh \$@" > $DIR/IGV/igv
     chmod +x $DIR/IGV/igv
     export PATH=$DIR/IGV:$PATH
     command -v igv >/dev/null 2>&1 || { echo "IGV still isn't on the path, try installing manually"; exit 1; }
