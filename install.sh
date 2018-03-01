d4e38d9b90aae35a0d317098477579b443a549bb
Erik Clarke <ecl@mail.med.upenn.edu>
2018-03-01 16:15:50 -0500

    modularized install

--- install.sh
+++ install.sh
@@ -16,22 +16,25 @@ install_conda () {
     command -v conda >/dev/null 2>&1 || { echo "Conda still isn't on the path, try installing manually"; exit 1; }
 }
 
+# Install conda if it doesn't show up on the path
 command -v conda >/dev/null 2>&1 || { echo "Conda not installed, installing now"; install_conda; }
-
-conda config --add channels r
-conda config --add channels bioconda
-conda config --add channels eclarke
-conda config --add channels conda-forge
 
 # Create the environment if it doesn't yet exist
 conda env list | cut -f1 -d' ' | grep -Fxq $SUNBEAM_ENV_NAME || {
+    
+    conda config --add channels r
+    conda config --add channels bioconda
+    conda config --add channels eclarke
+    conda config --add channels conda-forge
     conda create --name=$SUNBEAM_ENV_NAME --file=conda-requirements.txt --quiet --yes >> $OUTPUT
+    
 }
 
 # Install sunbeam module 
 command -v sunbeam_init >/dev/null 2>&1 || {
     source activate $SUNBEAM_ENV_NAME
     pip install --upgrade --editable . >> $OUTPUT
+    command -v conda >/dev/null 2>&1 || { echo "Couldn't install sunbeam; please report this as a bug."; exit 1; }
     echo "Sunbeam successfully installed.";
 }
 
