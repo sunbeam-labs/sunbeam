ee342e439e446a379e48d825a1f12cff380dc28d
Erik <ecl@mail.med.upenn.edu>
2018-03-16 16:31:10 -0400

    fixed #119

--- install.sh
+++ install.sh
@@ -132,8 +132,8 @@ function install_conda () {
 }
 
 function install_environment () {
-    debug_capture $(conda env update --name=$__sunbeam_env \
-			  --quiet --file environment.yml)
+    debug_capture conda env update --name=$__sunbeam_env \
+			  --quiet --file environment.yml
     if [[ $(__test_env) != true ]]; then
 	installation_error "Environment creation"
     fi
