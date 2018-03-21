5ae5586dc91541ad0c040d8820203cd6a0ef2f33
Erik Clarke <ecl@mail.med.upenn.edu>
2018-03-21 14:05:13 -0400

    cleanup

--- sunbeamlib/reports.py
+++ sunbeamlib/reports.py
@@ -1,16 +1,11 @@
-import warnings
-warnings.filterwarnings('ignore', '.*experimental.*')
 from pathlib import Path
 from collections import Counter
 
 from Bio import SeqIO
-
 from Bio import SearchIO
 from Bio.SeqRecord import SeqRecord
 
 from xml.etree.ElementTree import ParseError
-
-from .parsers import MetaGeneAnnotation
 
 def blast_summary(blast_files, blast_format="blast-xml"):
     """Summarize BLAST results from an set of BLAST output files."""
@@ -34,27 +29,6 @@ def blast_contig_summary(xml_files):
 def blast_hits(blast_xml_files):
     return Counter(c['query'] for c in blast_summary(blast_xml_files))
     
-def mga_summary(orf_fp, contigs_fp, sample_id):
-    """Summarizes results from MetaGene Annotator."""
-    genes = []
-    gene_no = 0
-    contigs = SeqIO.parse(contigs_fp, 'fasta')
-    contig_seqs = {r.description: r.seq for r in contigs}
-    annotations = MetaGeneAnnotation.parse(open(orf_fp))
-    for anno in annotations:
-        contig_seq = contig_seqs[anno.id]
-        # Gather putative genes and create SeqRecords for them
-        for pgene in anno.genes:
-            gene_seq = contig_seq[pgene.start:pgene.end]
-            if pgene.strand == '-':
-                gene_seq = gene_seq.reverse_complement()
-            gene = SeqRecord(
-                gene_seq,
-                id=anno.id,
-                description="{}:{}".format(sample_id, gene_no))
-            genes.append(gene)
-            gene_no += 1
-    return genes
 
 
         
