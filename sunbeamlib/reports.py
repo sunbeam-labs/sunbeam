d0b5b9ed830ec4d12eb980326ee0cb1e314e1242
Erik Clarke <ecl@mail.med.upenn.edu>
2018-05-22 14:06:01 -0400

    refactor reports.rules

--- sunbeamlib/reports.py
+++ sunbeamlib/reports.py
@@ -30,6 +30,57 @@ def blast_contig_summary(xml_files):
 
 def blast_hits(blast_xml_files):
     return Counter(c['query'] for c in blast_summary(blast_xml_files))
+
+def parse_trim_summary_paired(f):
+    for line in f.readlines():
+        if line.startswith('Input Read'):
+            vals = re.findall('\D+\: (\d+)', line)
+            keys = ('input', 'both_kept','fwd_only','rev_only','dropped')
+            return(OrderedDict(zip(keys, vals)))
+
+def parse_trim_summary_single(f):
+    for line in f:
+        if line.startswith('Input Read'):
+            vals = re.findall('\D+\: (\d+)', line)
+            keys = ('input', 'kept', 'dropped')
+            return(OrderedDict(zip(keys, vals)))
+
+def parse_decontam_log(f):
+    keys = f.readline().rstrip().split('\t')
+    vals = f.readline().rstrip().split('\t')
+    return(OrderedDict(zip(keys,vals)))
+
+def summarize_qual_decontam(tfile, dfile):
+    """Return a dataframe for summary information for trimmomatic and decontam rule"""
+    tname = os.path.basename(tfile).split('.out')[0]
+    dname = os.path.basename(dfile).split('.txt')[0]
+    with open(tfile) as tf:
+        with open(dfile) as jf:
+            if Cfg['all']['paired_end']:
+                trim_data = parse_trim_summary_paired(tf)
+            else:
+                trim_data = parse_trim_summary_single(tf)
+                
+            decontam_data = parse_decontam_log(jf)
+    sys.stderr.write("trim data: {}\n".format(trim_data))
+    sys.stderr.write("decontam data: {}\n".format(decontam_data))
+    return(pandas.DataFrame(OrderedDict(trim_data, **(decontam_data)), index=[tname]))
+
+def parse_fastqc_quality(filename):
+    with open(filename) as f:
+        report = f.read()
+    tableString = re.search(
+        '\>\>Per base sequence quality.*?\n(.*?)\n\>\>END_MODULE',
+        report, re.DOTALL).group(1)
+
+    f_s = StringIO(tableString)
+    df = pandas.read_csv(
+        f_s, sep='\t', usecols=['#Base', 'Mean'], index_col='#Base')
+    sample_name = os.path.basename(filename.split('_fastqc')[0])
+    df.columns=[sample_name]
+    f_s.close()
+    return(df)
+
     
 
 
