9edd88e3bdafe9f47a9d055595747f2ba5aba1e7
Jesse Connell <ancon@upenn.edu>
2018-11-30 14:45:01 -0500

    Move sunbeam get features into sunbeam init
    This uses sunbeam init as the entry point for any SRA-listing features,
    instead of a separate module.  Utility functions have been added to
    list_samples.py alongside their local file list equivalents.

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     entry_points={'console_scripts': [
         'sunbeam = sunbeamlib.scripts.command:main',
         'sunbeam_init = sunbeamlib.scripts.init:main',
-        'sunbeam_get = sunbeamlib.scripts.get:main',
         'sunbeam_mod_config = sunbeamlib.scripts.config:main'
     ]},
     classifiers=[
