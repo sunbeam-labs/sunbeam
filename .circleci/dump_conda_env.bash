2c1dfe0d8c81c8bb00ce4f142c8384470499037d
Louis Taylor <louiejtaylor@gmail.com>
2019-11-26 12:30:33 -0500

    allow conda activate in envdump script

--- .circleci/dump_conda_env.bash
+++ .circleci/dump_conda_env.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-conda activate sunbeam
+# Allow conda [de]activate
+export PATH=$PATH:$HOME/miniconda3/bin
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
 
+# Dump environment contents
+conda activate sunbeam
 conda list --export
