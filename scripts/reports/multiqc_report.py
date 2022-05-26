fbd0fc3ec83715dd11d85535e5a111d650625b55
ctbus <ctbushman@gmail.com>
2022-05-26 15:44:20 -0400

    Testing multiqc

--- scripts/reports/multiqc_report.py
+++ scripts/reports/multiqc_report.py
@@ -4,6 +4,9 @@ report_name = snakemake.output[0].split('/')[-1]  # Get unique name from targets
 
 os.system("multiqc -i \"{a}\" -n {b} -o {c} {c}".format(
             a=snakemake.params.title, b=report_name, c=snakemake.params.outdir))
+os.system(f"ls -al {snakemake.params.outdir}")
+os.system(f"echo {report_name}")
+os.system(f"echo {snakemake.output[0]}")
 if not os.path.exists(snakemake.output[0]):
     os.rename(os.path.join(snakemake.params.outdir, report_name),
             snakemake.output[0])
