ddd4aa560dea8e62e50f484f64dc4f5cc2cf24c5
ctbus <ctbushman@gmail.com>
2022-05-27 09:34:25 -0400

    Comment

--- scripts/reports/multiqc_report.py
+++ scripts/reports/multiqc_report.py
@@ -4,4 +4,4 @@ report_name = snakemake.output[0].split('/')[-1]  # Get unique name from targets
 
 os.system("multiqc -f -i \"{a}\" -n {b} -o {c} {c}".format(
             a=snakemake.params.title, b=report_name, c=snakemake.params.outdir))
-
+# -f overwrites any previous reports instead of iterating the next one's name
