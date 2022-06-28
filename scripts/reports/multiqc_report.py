8f3b077f4ecdb011e9df91c16d189eb0cf158d6f
Charlie <ctbushman@gmail.com>
2022-06-27 22:04:04 -0400

    Update multiqc_report.py

--- scripts/reports/multiqc_report.py
+++ scripts/reports/multiqc_report.py
@@ -5,8 +5,3 @@ report_name = snakemake.output[0].split('/')[-1]  # Get unique name from targets
 os.system("multiqc -f -i \"{a}\" -n {b} -o {c} {c}".format(
             a=snakemake.params.title, b=report_name, c=snakemake.params.outdir))
 # -f overwrites any previous reports instead of iterating the next one's name
-
-# Different versions of multiqc seem to be inconsistent with naming conventions
-for fp in os.listdir(snakemake.params.outdir):
-    if 'multiqc' in fp:
-        os.replace(os.path.join(snakemake.params.outdir, fp), snakemake.output[0])
