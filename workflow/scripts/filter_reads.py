ed411dec95d25c0d9d5a95e75cf69be3f20e70a0
ctbus <ctbushman@gmail.com>
2023-01-26 15:21:19 -0500

    Remove sys pipes

--- workflow/scripts/filter_reads.py
+++ workflow/scripts/filter_reads.py
@@ -4,7 +4,6 @@ from collections import OrderedDict
 from filter_reads_f import count_host_reads, calculate_counts, write_log
 
 with open(snakemake.log[0], "w") as l:
-    sys.stderr = sys.stdout = l
     hostdict = OrderedDict()
     net_hostlist = set()
     for hostid in snakemake.input.hostids:
@@ -12,23 +11,19 @@ with open(snakemake.log[0], "w") as l:
 
     host, nonhost = calculate_counts(snakemake.input.reads, net_hostlist)
 
-    try:
-        sp.check_output(
-            "gzip",
-            "-dc",
-            snakemake.input.reads,
-            "|",
-            "rbt",
-            "fastq-filter",
-            snakemake.input.hostreads,
-            "|",
-            "gzip",
-            ">",
-            snakemake.output.reads,
-        )
-    except sp.CalledProcessError as e:
-        sys.stderr.write(f"Error on filter_reads for {snakemake.input.reads} and {snakemake.input.hostreads}")
-        sys.exit(e)
+    sp.check_output(
+        "gzip",
+        "-dc",
+        snakemake.input.reads,
+        "|",
+        "rbt",
+        "fastq-filter",
+        snakemake.input.hostreads,
+        "|",
+        "gzip",
+        ">",
+        snakemake.output.reads,
+    )
 
     with open(snakemake.output.log, "w") as log:
         write_log(log, hostdict, host, nonhost)
