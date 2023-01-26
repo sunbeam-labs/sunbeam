e09c117283f7dce8cb2bd5b09c9235f8c583d947
ctbus <ctbushman@gmail.com>
2023-01-26 16:12:01 -0500

    Switch to sp.call

--- workflow/scripts/filter_reads.py
+++ workflow/scripts/filter_reads.py
@@ -11,7 +11,7 @@ with open(snakemake.log[0], "w") as l:
 
     host, nonhost = calculate_counts(snakemake.input.reads, net_hostlist)
 
-    sp.run(
+    sp.call(
         "gzip",
         "-dc",
         snakemake.input.reads,
