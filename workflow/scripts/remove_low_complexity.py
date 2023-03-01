ea69bbc9644e3c87c0130ea2553ac01d2fb3e8a9
ctbus <ctbushman@gmail.com>
2023-03-01 12:31:19 -0500

    WORKING????

--- workflow/scripts/remove_low_complexity.py
+++ workflow/scripts/remove_low_complexity.py
@@ -1,7 +1,9 @@
 from sunbeamlib.qc import filter_ids
 
-ids = []
-with open(snakemake.input.ids) as f:
-    ids = [id.strip() for id in f.readlines()]
+with open(snakemake.log[0], "w") as log:
+    ids = []
+    with open(snakemake.input.ids) as f:
+        ids = [id.strip()[:-1] for id in f.readlines()]
+    log.write(f"Komplexity IDs to be filtered: {str(ids)}\n")
 
-filter_ids(snakemake.input.reads, snakemake.output[0], ids)
+    filter_ids(snakemake.input.reads, snakemake.output[0], ids, log)
