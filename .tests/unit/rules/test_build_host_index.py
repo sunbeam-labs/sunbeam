7e1bd844557b0b8a7f07c28cb5da16f524c46b79
ctbus <ctbushman@gmail.com>
2023-03-03 10:27:00 -0500

    Add test_build_host_index

---
+++ .tests/unit/rules/test_build_host_index.py
@@ -0,0 +1,42 @@
+import os
+import pytest
+import shutil
+import subprocess as sp
+import sys
+from pathlib import Path
+from . import init
+
+test_dir = Path(__file__).parent.parent.parent.resolve()
+sys.path.append(str(test_dir))
+from config_fixture import output_dir, config
+
+data_dir = Path(__file__).parent / "data"
+
+
+@pytest.fixture
+def setup(init):
+    output_dir = init
+
+    yield output_dir
+
+    shutil.rmtree(output_dir / "sunbeam_output")
+
+
+def test_build_host_index(setup):
+    output_dir = setup
+    sunbeam_output_dir = output_dir / "sunbeam_output"
+    exts = ["amb", "ann", "bwt", "pac", "sa"]
+    human = [test_dir / "data" / "hosts" / f"human.fasta.{ext}" for ext in exts]
+    phix = [test_dir / "data" / "hosts" / f"phix174.fasta.{ext}" for ext in exts]
+
+    args = ["sunbeam",
+            "run",
+            "--profile",
+            f"{output_dir}",
+            "--notemp",
+            "--rerun-triggers=input",]
+    args.append(human)
+    args.append(phix)
+    sp.check_output(args)
+
+    assert len(os.listdir(test_dir / "data" / "hosts")) == 12
