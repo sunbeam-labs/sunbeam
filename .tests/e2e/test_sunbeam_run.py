9b6520e19f77090e3a017143c391df34523fc245
ctbus <ctbushman@gmail.com>
2023-03-05 09:59:50 -0500

    Add single end sunbeam run test

--- .tests/e2e/test_sunbeam_run.py
+++ .tests/e2e/test_sunbeam_run.py
@@ -83,3 +83,85 @@ def test_sunbeam_run_all(init):
         )  # Human + phiX + komplexity
         assert int(stats[8]) + int(stats[10]) == 200  # Host + komplexity
         assert int(stats[9]) == 200  # Nonhost
+
+
+@pytest.fixture
+def init_single_end(output_dir):
+    output_dir = output_dir / "sunbeam_run_single_end"
+
+    reads_fp = test_dir / 'data' / 'single_end_reads'
+    reads_fp.mkdir(parents=True, exist_ok=True)
+    shutil.copyfile(test_dir / "data" / "reads" / "TEST_R1.fastq.gz", reads_fp / "TEST.fastq.gz")
+
+    sp.check_output(
+        [
+            "sunbeam",
+            "init",
+            "--data_fp",
+            f"{reads_fp}",
+            "--single_end",
+            output_dir,
+        ]
+    )
+
+    config_str = f"qc: {{host_fp: {test_dir / 'data' / 'hosts'}}}"
+
+    sp.check_output(
+        [
+            "sunbeam",
+            "config",
+            "modify",
+            "-i",
+            "-s",
+            f"{config_str}",
+            f"{output_dir / 'sunbeam_config.yml'}",
+        ]
+    )
+
+    yield output_dir
+
+    shutil.rmtree(reads_fp)
+
+    if os.environ.get("CI", False):
+        try:
+            shutil.copytree(output_dir, "output_sunbeam_run/")
+        except FileExistsError as e:
+            pass
+
+
+def test_sunbeam_run_all_single_end(init):
+    output_dir = init
+    sunbeam_output_dir = output_dir / "sunbeam_output"
+
+    sp.check_output(
+        [
+            "sunbeam",
+            "run",
+            "--profile",
+            f"{output_dir}",
+            "--notemp",
+        ]
+    )
+
+    with open(test_dir / "targets.txt") as f:
+        for line in f.readlines():
+            if not line.strip():
+                continue
+            target = sunbeam_output_dir / line.strip()
+            if not target.exists():
+                raise SystemExit(f"Target '{target}' not found")
+            elif target.stat().st_size == 0:
+                raise SystemExit(f"Target '{target}' is empty")
+            else:
+                print(f"Found target '{target}'")
+
+    with open(sunbeam_output_dir / "qc" / "reports" / "preprocess_summary.tsv") as f:
+        f.readline()  # Headers
+        stats = f.readline().split("\t")
+        assert int(stats[1]) == 400  # Input reads
+        assert int(stats[2]) == 400  # Both kept by cutadapt
+        assert (
+            int(stats[6]) + int(stats[7]) + int(stats[10]) == 200
+        )  # Human + phiX + komplexity
+        assert int(stats[8]) + int(stats[10]) == 200  # Host + komplexity
+        assert int(stats[9]) == 200  # Nonhost
