75c3e3b64c1505ca09394e3e2281e6955bc9c928
ctbus <ctbushman@gmail.com>
2023-03-08 23:32:32 -0500

    Fix write_many_fastq

--- sunbeamlib/parse.py
+++ sunbeamlib/parse.py
@@ -39,6 +39,6 @@ def write_fastq(record, f):
 
 
 def write_many_fastq(record_list, f):
-    record_list = [[f"@{r[0]}", r[1], r[2], r[3]] for r in record_list]
+    record_list = [[f"@{r[0]}\n", f"{r[1]}\n", f"{r[2]}\n", f"{r[3]}\n"] for r in record_list]
     record_list = [item for sublist in record_list for item in sublist]
     f.writelines(record_list)
