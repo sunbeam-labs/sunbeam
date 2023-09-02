195468a29bcc4e587cc0426b8aeac62eb6cba9cd
ctbus <ctbushman@gmail.com>
2023-09-02 17:40:07 -0400

    Explicitly require theme

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_tabs.tabs']
+extensions = ['sphinx_rtd_theme', 'sphinx_tabs.tabs']
 
 sphinx_tabs_disable_tab_closing = True
 
