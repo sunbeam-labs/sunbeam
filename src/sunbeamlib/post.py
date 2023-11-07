c109000df7f48cb8491a46f7003248f4c8846e7b
ctbus <ctbushman@gmail.com>
2023-11-07 11:56:06 -0500

    Working improved stats

--- src/sunbeamlib/post.py
+++ src/sunbeamlib/post.py
@@ -2,10 +2,11 @@ import csv
 import datetime
 import os
 from pathlib import Path
+from snakemake.common import Rules
 from typing import Dict
 
 
-def compile_benchmarks(benchmark_fp: str, Cfg: Dict[str, Dict | str]) -> None:
+def compile_benchmarks(benchmark_fp: str, Cfg: Dict[str, Dict | str], rules: Rules) -> None:
     """Aggregate all the benchmark files into one and put it in stats_fp"""
     stats_fp = Path(Cfg["all"]["root"]) / "stats"
     benchmarks = []
@@ -37,15 +38,48 @@ def compile_benchmarks(benchmark_fp: str, Cfg: Dict[str, Dict | str]) -> None:
                 next(reader)  # Headers line
                 writer.writerow([fp[:-4]] + next(reader))
     
-    compile_file_stats(stats_fp, Cfg, dt)
+    compile_file_stats(stats_fp, Cfg, dt, rules)
 
 
-def compile_file_stats(stats_fp: str, Cfg: Dict[str, Dict | str], dt: str) -> None:
+def compile_file_stats(stats_fp: str, Cfg: Dict[str, Dict | str], dt: str, rules: Rules) -> None:
     """Collect data on all inputs and outputs (as long as they still exist at this point) as well as dbs"""
     file_stats = {}
     output_fp = Path(Cfg["all"]["root"]) / Cfg["all"]["output_fp"]
 
     stats_file = os.path.join(stats_fp, f"{dt}_file_stats.tsv")
+
+    # Collect info on all files in params
+    param_fps = set()
+    for n, r in rules._rules.items():
+        if r.params:
+            for p in r.params:
+                if isinstance(p, dict):
+                    for k, v in p.items():
+                        try:
+                            param_fps.add(Path(v))
+                        except TypeError:
+                            continue
+                elif isinstance(p, list):
+                    for i in p:
+                        try:
+                            param_fps.add(Path(i))
+                        except TypeError:
+                            continue
+                else:
+                    try:
+                        param_fps.add(Path(p))
+                    except TypeError:
+                        continue
+    
+    for fp in param_fps:
+        if fp.exists():
+            if fp.is_file():
+                file_stats[fp] = fp.stat().st_size
+            elif fp.is_dir():
+                for file in os.listdir(fp):
+                    nfp = fp / file
+                    if nfp.exists() and nfp.is_file():
+                        file_stats[nfp] = nfp.stat().st_size
 
     # Collect info on all files in output_fp
     for root, dirs, files in os.walk(output_fp):
@@ -57,8 +91,7 @@ def compile_file_stats(stats_fp: str, Cfg: Dict[str, Dict | str], dt: str) -> No
     for section, values in Cfg.items():
         if section == "all":
             continue
-        print(values)
-        for v in values:
+        for k, v in values.items():
             try:
                 vp = Path(v)
             except TypeError:
