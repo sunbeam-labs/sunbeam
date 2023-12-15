d684c421ab2fc8378be37811c1f15071b87d8086
Naomi Wilson <wilsonn4@chop.edu>
2023-12-15 11:13:08 -0500

    strip dashes added by grep -A and confirm expected length

--- workflow/scripts/remove_low_complexity_grep.sh
+++ workflow/scripts/remove_low_complexity_grep.sh
@@ -21,10 +21,44 @@ grep -v -f ${ids}_unique ${log_fp}/${SAMPLEID}.trimm_ids > ${log_fp}/${SAMPLEID}
 echo "filter reads with zgrep"
 komp_fp="$(dirname "${out1}")"
 mkdir -p $komp_fp &>/dev/null # be silent
-zgrep -A 3 -f ${log_fp}/${SAMPLEID}.komplexity_keep_ids $read1 | gzip > $out1
-zgrep -A 3 -f ${log_fp}/${SAMPLEID}.komplexity_keep_ids $read2 | gzip > $out2
+zgrep -A 3 -f ${log_fp}/${SAMPLEID}.komplexity_keep_ids $read1 | sed '/^--$/d' | gzip > $out1
+zgrep -A 3 -f ${log_fp}/${SAMPLEID}.komplexity_keep_ids $read2 | sed '/^--$/d' | gzip > $out2
 
 exitcode=$?
+
+newheaders=$( zgrep -c "^@" $out1 )
+newlines=$( zcat $out1 | wc -l )
+numids=$(< ${log_fp}/${SAMPLEID}.komplexity_keep_ids wc -l )
+explines=$(( "$numids" + "$numids" + "$numids" + "$numids" ))
+echo $newheaders
+echo $newlines
+echo $explines
+echo $numids
+if [ "$newheaders" -eq "$numids" ]; then
+	if [ "$newlines" -eq "$explines" ]; then
+		continue
+	fi
+else	
+	exitcode=$(( "$exitcode" + 1 ))	
+	echo "Your filtered read 1 file does not equal the expected length"
+fi
+
+newheaders=$( zgrep -c "^@" $out2 )
+newlines=$( zcat $out2 | wc -l )
+numids=$(< ${log_fp}/${SAMPLEID}.komplexity_keep_ids wc -l )
+explines=$(( "$numids" + "$numids" + "$numids" + "$numids" ))
+echo $newheaders
+echo $newlines
+echo $explines
+echo $numids
+if [ "$newheaders" -eq "$numids" ]; then
+	if [ "$newlines" -eq "$explines" ]; then
+		continue
+	fi
+else	
+	exitcode=$(( "$exitcode" + 1 ))	
+	echo "Your filtered read 2 file does not equal the expected length"
+fi
 
 if [ $exitcode -eq 1 ]
 then
