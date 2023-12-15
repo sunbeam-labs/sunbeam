94edb88356bf9b1e22da37b8dacb78e3dac1c493
Naomi Wilson <wilsonn4@chop.edu>
2023-12-15 16:57:16 -0600

    fixed indents

--- src/sunbeamlib/qc.py
+++ src/sunbeamlib/qc.py
@@ -17,15 +17,15 @@ def filter_ids(fp_in: Path, fp_out: Path, ids: List[str], log: TextIO) -> None:
     ids: list of ids to be removed
     """
     with gzip.open(fp_in, "rt") as f_in, gzip.open(fp_out, "wt") as f_out:
-	  ids = set(ids)
-	  for record in parse_fastq(f_in):
-		record_id=record[0].split(" ")[0]
-		record_id=re.sub('\/1', '', record_id)
-		record_id=re.sub('\/2', '', record_id)
-		if not record[0] in ids:
-			write_fastq(record, f_out)
-		else:
-			log.write(f"{record[0]} filtered\n")		
+        ids = set(ids)
+        for record in parse_fastq(f_in):
+            record_id=record[0].split(" ")[0]
+            record_id=re.sub('\/1', '', record_id)
+            record_id=re.sub('\/2', '', record_id)
+                if not record_id in ids:
+                    write_fastq(record, f_out)
+                else:
+                    log.write(f"{record[0]} filtered\n")		
 
 
 def remove_pair_id(id: str, log: TextIO) -> str:
