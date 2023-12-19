2de153b7608f0ba85362b88e62d3d5bb80561018
Naomi Wilson <wilsonn4@chop.edu>
2023-12-19 00:55:17 -0500

    tweak the grep command to try and use less memory

--- workflow/scripts/remove_low_complexity_grep.sh
+++ workflow/scripts/remove_low_complexity_grep.sh
@@ -14,15 +14,15 @@ SAMPLEID=${base_name%.fastq.gz}
 #echo $NAME
 echo $read1 &>> $log
 echo $SAMPLEID &>> $log
-zgrep "^@" $read1 > ${log_fp}/${SAMPLEID}.trimm_verbose_ids
+zgrep -F "^@" $read1 > ${log_fp}/${SAMPLEID}.trimm_verbose_ids
 sed 's/ .*$//g' ${log_fp}/${SAMPLEID}.trimm_verbose_ids | sed 's/\/[1-2]$//g' | sort -u > ${log_fp}/${SAMPLEID}.trimm_ids
 sed 's/ .*$//g' ${ids} | sed 's/\/[1-2]$//g' | sort -u > ${ids}_unique
-grep -v -f ${ids}_unique ${log_fp}/${SAMPLEID}.trimm_ids > ${log_fp}/${SAMPLEID}.komplexity_keep_ids
+grep -vF -f ${ids}_unique ${log_fp}/${SAMPLEID}.trimm_ids > ${log_fp}/${SAMPLEID}.komplexity_keep_ids
 komp_fp="$(dirname "${out1}")"
 mkdir -p $komp_fp &>/dev/null # be silent
-zgrep -A 3 -f ${log_fp}/${SAMPLEID}.komplexity_keep_ids $read1 | sed '/^--$/d' | gzip > $out1
-mistakes=$( zgrep -c -f $ids $out1 ) 2>/dev/null
-newheaders=$( zgrep -c "^@" $out1 )
+zgrep -F -A 3 -f ${log_fp}/${SAMPLEID}.komplexity_keep_ids $read1 | sed '/^--$/d' | gzip > $out1
+mistakes=$( zgrep -F -c -f -m1 $ids $out1 ) 2>/dev/null
+newheaders=$( zgrep -cF "^@" $out1 )
 newlines=$( zcat $out1 | wc -l )
 numids=$(< ${log_fp}/${SAMPLEID}.komplexity_keep_ids wc -l )
 explines=$(( "$numids" + "$numids" + "$numids" + "$numids" ))
