3ffb16cbf2da4df944dd67b5aaffcdc716ed17b1
Ulthran <ctbushman@gmail.com>
2023-12-27 11:15:47 -0500

    Reformat

--- src/sunbeamlib/resources.py
+++ src/sunbeamlib/resources.py
@@ -1,14 +1,14 @@
 import os
 
-MIN_MEM_MB = os.getenv("SUNBEAM_MIN_MEM_MB") if os.getenv("SUNBEAM_MIN_MEM_MB") else 1000
-MIN_RUNTIME = os.getenv("SUNBEAM_MIN_RUNTIME") if os.getenv("SUNBEAM_MIN_RUNTIME") else 20
+MIN_MEM_MB = os.getenv("SUNBEAM_MIN_MEM_MB", 1000)
+MIN_RUNTIME = os.getenv("SUNBEAM_MIN_RUNTIME", 20)
 
-#def adapter_removal_mem_mb(attempts):
+# def adapter_removal_mem_mb(attempts):
 
 
 def sample_intake_mem_mb(wildcards, attempt):
     return MIN_MEM_MB
 
+
 def sample_intake_runtime(wildcards, attempt):
     return MIN_RUNTIME
-
