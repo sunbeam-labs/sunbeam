610ae8aceef75ce63e9de0e64be05d2a827c604a
Ulthran <ctbushman@gmail.com>
2024-03-12 15:47:52 -0600

    Copy instead of symlink for sample intake

--- workflow/scripts/sample_intake.py
+++ workflow/scripts/sample_intake.py
@@ -1,6 +1,8 @@
+import shutil
 from pathlib import Path
 
 with open(snakemake.log[0], "w") as log:
     assert snakemake.input[0].endswith(".fastq.gz")
     log.write("Creating symlink\n")
-    Path(snakemake.output[0]).symlink_to(snakemake.input[0])
+    # Path(snakemake.output[0]).symlink_to(snakemake.input[0])
+    shutil.copy(snakemake.input[0], snakemake.output[0])
