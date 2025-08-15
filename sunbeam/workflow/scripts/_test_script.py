4130e3f00bd8420b54d1b7163318d2b556ace1a4
Ulthran <ctbushman@gmail.com>
2025-08-15 11:02:39 -0700

    Add more slurm tests

---
+++ sunbeam/workflow/scripts/_test_script.py
@@ -0,0 +1,8 @@
+from sunbeam.logging import get_pipeline_logger, get_extension_logger
+from sunbeam.project import SampleList, SunbeamConfig, output_subdir
+
+
+logger = get_pipeline_logger()
+logger.info("This works!")
+
+SampleList()
